573f116cfd6f14427d989cbe8514e3615685933d
pablo <olbaplegal@gmail.com>
2026-02-16 15:10:42 -0300

    feat: created method delete

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,19 @@ const app = express();
 const PORT = 5001
 
 app.get("/api/notes", (req,res) => {
-    res.send("you got 11 notes");
+    res.status(200).send("you got 11 notes");
 })
 
 app.post("/api/notes", (req,res) => {
-    res.status(201).json({message:"post created successfully!"})
+    res.status(201).json({message:"Note created successfully!"})
 })
 
 app.put("/api/notes/:id", (req,res) => {
-    res.status(200).json({message:"post updated successfully!"})
+    res.status(200).json({message:"Note updated successfully!"})
+})
+
+app.delete("/api/notes/:id", (req,res) => {
+    res.status(200).json({message:"Note updated successfully!"})
 })
 
 app.listen(PORT, () => {
