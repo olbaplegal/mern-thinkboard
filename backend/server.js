bea5e8a30c52b4555b82e0cd1d1a4e4e9fa0fdba
pablo <olbaplegal@gmail.com>
2026-02-16 15:07:50 -0300

    feat: method post created

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ app.get("/api/notes", (req,res) => {
     res.send("you got 11 notes");
 })
 
+app.post("/api/notes", (req,res) => {
+    res.status(201).json({message:"post created successfully!"})
+})
+
 app.listen(PORT, () => {
     console.log(`Server started on PORT: ${PORT}`)
 })
